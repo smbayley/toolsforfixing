718739b70626a2e40c4782691f7953123ff31fdd
Sean Bayley <seanbayley@gmail.com>
2019-08-21 15:36:38 -0700

    Updates

---
+++ precache-manifest.ba5dbc4a79325d524749d63c7f034b87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4267b41ba6bdd40c4cd52a7c8997b1e5",
+    "url": "/fixit-devices/index.html"
+  },
+  {
+    "revision": "81885a28967a67384e9b",
+    "url": "/fixit-devices/static/css/main.eee420e0.chunk.css"
+  },
+  {
+    "revision": "0fe1afe9f7910599d7f3",
+    "url": "/fixit-devices/static/js/2.16793f06.chunk.js"
+  },
+  {
+    "revision": "81885a28967a67384e9b",
+    "url": "/fixit-devices/static/js/main.4eeb26eb.chunk.js"
+  },
+  {
+    "revision": "967a14ef409806a910d5",
+    "url": "/fixit-devices/static/js/runtime~main.8c885d43.js"
+  }
+]);
